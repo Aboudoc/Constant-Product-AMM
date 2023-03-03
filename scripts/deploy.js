f2e2082d554c79262f9043ea8660bdab7c68ee6b
Aboudoc <aboutika.reda@gmail.com>
2023-03-03 17:12:52 +0100

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,10 +3,7 @@ const hre = require("hardhat");
 require("dotenv").config();
 
 async function main() {
-  // const king = await hre.ethers.getContractAt("King", instanceAddress)
-
   const UniswapV2Twap = await ethers.getContractFactory("UniswapV2Twap");
-  // send more than 0,01ETH (King's contract balance) when deploying
   const uniswapV2Twap = await UniswapV2Twap.deploy(process.env.PAIR);
 
   await uniswapV2Twap.deployed();
