88d0189e8a791947075617de26786a0d4d11c7e8
Aboudoc <aboutika.reda@gmail.com>
2023-03-04 15:56:27 +0100

    mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
@@ -13,11 +14,16 @@ module.exports = {
       { version: "0.7.6" },
     ],
   },
-  dafaultNetwork: "localhost",
+  dafaultNetwork: "hardhat",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
   networks: {
+    hardhat: {
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+    },
     goerli: {
       url: GOERLI_RPC_URL,
       accounts: [PRIVATE_KEY],
